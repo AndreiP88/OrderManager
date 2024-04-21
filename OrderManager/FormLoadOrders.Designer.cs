f0e21452e89dd56e78b6ba29a19cb1dd7d869f2b
Андрей Павельчук <andrei.foxoxy@gmail.com>
2024-04-21 19:03:58 +0300

    Список заказов выходил за пределы окна

--- OrderManager/FormLoadOrders.Designer.cs
+++ OrderManager/FormLoadOrders.Designer.cs
@@ -83,7 +83,7 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 48F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 5F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1330, 565);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1263, 565);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // tableLayoutPanel2
@@ -101,15 +101,15 @@
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(1318, 44);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1251, 44);
             this.tableLayoutPanel2.TabIndex = 7;
             // 
             // button4
             // 
             this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button4.Location = new System.Drawing.Point(1056, 3);
+            this.button4.Location = new System.Drawing.Point(1003, 3);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(259, 38);
+            this.button4.Size = new System.Drawing.Size(245, 38);
             this.button4.TabIndex = 1;
             this.button4.Text = "Отмена";
             this.button4.UseVisualStyleBackColor = true;
@@ -119,9 +119,9 @@
             // 
             this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.button3.Enabled = false;
-            this.button3.Location = new System.Drawing.Point(793, 3);
+            this.button3.Location = new System.Drawing.Point(753, 3);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(257, 38);
+            this.button3.Size = new System.Drawing.Size(244, 38);
             this.button3.TabIndex = 0;
             this.button3.Text = "Добавить";
             this.button3.UseVisualStyleBackColor = true;
@@ -139,16 +139,16 @@
             this.tableLayoutPanel4.Name = "tableLayoutPanel4";
             this.tableLayoutPanel4.RowCount = 1;
             this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel4.Size = new System.Drawing.Size(784, 38);
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(744, 38);
             this.tableLayoutPanel4.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label2.Location = new System.Drawing.Point(257, 0);
+            this.label2.Location = new System.Drawing.Point(244, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(524, 38);
+            this.label2.Size = new System.Drawing.Size(497, 38);
             this.label2.TabIndex = 1;
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
@@ -158,7 +158,7 @@
             this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label1.Location = new System.Drawing.Point(3, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(248, 38);
+            this.label1.Size = new System.Drawing.Size(235, 38);
             this.label1.TabIndex = 2;
             this.label1.Text = "label1";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -181,7 +181,7 @@
             this.tableLayoutPanel3.RowCount = 2;
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel3.Size = new System.Drawing.Size(1316, 58);
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(1249, 58);
             this.tableLayoutPanel3.TabIndex = 9;
             // 
             // comboBox1
@@ -191,7 +191,7 @@
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(3, 3);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(520, 21);
+            this.comboBox1.Size = new System.Drawing.Size(493, 21);
             this.comboBox1.TabIndex = 9;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
@@ -200,7 +200,7 @@
             this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.textBox1.Location = new System.Drawing.Point(3, 32);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(520, 20);
+            this.textBox1.Size = new System.Drawing.Size(493, 20);
             this.textBox1.TabIndex = 8;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
@@ -209,10 +209,10 @@
             // 
             this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.button1.Enabled = false;
-            this.button1.Location = new System.Drawing.Point(528, 31);
+            this.button1.Location = new System.Drawing.Point(501, 31);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(193, 25);
+            this.button1.Size = new System.Drawing.Size(183, 25);
             this.button1.TabIndex = 0;
             this.button1.Text = "Поиск";
             this.button1.UseVisualStyleBackColor = true;
@@ -221,10 +221,10 @@
             // button2
             // 
             this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button2.Location = new System.Drawing.Point(528, 2);
+            this.button2.Location = new System.Drawing.Point(501, 2);
             this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(193, 25);
+            this.button2.Size = new System.Drawing.Size(183, 25);
             this.button2.TabIndex = 1;
             this.button2.Text = "Загрузить план";
             this.button2.UseVisualStyleBackColor = true;
@@ -252,7 +252,7 @@
             this.listView1.MultiSelect = false;
             this.listView1.Name = "listView1";
             this.listView1.ShowItemToolTips = true;
-            this.listView1.Size = new System.Drawing.Size(1316, 442);
+            this.listView1.Size = new System.Drawing.Size(1249, 442);
             this.listView1.TabIndex = 10;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -308,13 +308,13 @@
             // columnHeader10
             // 
             this.columnHeader10.Text = "Штамп";
-            this.columnHeader10.Width = 198;
+            this.columnHeader10.Width = 120;
             // 
             // FormLoadOrders
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1330, 565);
+            this.ClientSize = new System.Drawing.Size(1263, 565);
             this.Controls.Add(this.tableLayoutPanel1);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
